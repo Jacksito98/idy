be995ad8a00cbbe94604488a0a1da9311d6cf12c
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-08 15:33:17 -0400

    Add files via upload

---
+++ plugins/main-dylux.js
@@ -0,0 +1,27 @@
+
+let handler = async (m, { conn }) => {
+
+m.reply(`
+≡  *idyᴮᴼᵀ ┃ SUPPORT*
+
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
+▢ Grupo *1*
+https://chat.whatsapp.com
+
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
+
+▢ *Telegram*
+• https://t.me/JacksitoR
+
+ ▢ *PayPal*
+• https://paypal.me/Jacksito23
+
+▢ *YouTube*
+• https://www.youtube.com`)
+
+}
+handler.help = ['support']
+handler.tags = ['main']
+handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux', 'support'] 
+
+export default handler
