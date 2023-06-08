11bb59786d615e076369c14a64e6175532a41615
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-08 16:57:23 -0400

    Update game-ppt.js

--- plugins/game-ppt.js
+++ plugins/game-ppt.js
@@ -17,30 +17,30 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     if (text == astro) {
       global.db.data.users[m.sender].exp += 100
-        m.reply(`▢ *Empate*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Puntos (±)100 XP`)
+        m.reply(`▢ *Empate*\n\n‣ Tú : ${text}\n‣ idy : ${astro}\n\n🎁 Puntos (±)100 XP`)
     } else if (text == 'piedra') {
         if (astro == 'tijera') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *Ganaste* 🎊\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Puntos *+${poin} XP*`)
+            m.reply(`▢ *Ganaste* 🎊\n\n‣ Tú : ${text}\n‣ idy : ${astro}\n\n🎁 Puntos *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *Perdiste*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n Puntos *-${poin} XP*`)
+            m.reply(`▢ *Perdiste*\n\n‣ Tú : ${text}\n‣ idy : ${astro}\n\n Puntos *-${poin} XP*`)
         }
     } else if (text == 'tijera') {
         if (astro == 'papel') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *Ganaste* 🎊\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Puntos *+${poin} XP*`)
+            m.reply(`▢ *Ganaste* 🎊\n\n‣ Tú : ${text}\n‣ idy : ${astro}\n\n🎁 Puntos *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *Perdiste*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\nPuntos *-${poin} XP*`)
+            m.reply(`▢ *Perdiste*\n\n‣ Tú : ${text}\n‣ idy : ${astro}\n\nPuntos *-${poin} XP*`)
         }
     } else if (text == 'papel') {
         if (astro == 'piedra') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *Ganaste* 🎊\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Puntos *+${poin} XP*`)
+            m.reply(`▢ *Ganaste* 🎊\n\n‣ Tú : ${text}\n‣ idy : ${astro}\n\n🎁 Puntos *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *Perdiste*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\nPuntos *-${poin} XP*`)
+            m.reply(`▢ *Perdiste*\n\n‣ Tú : ${text}\n‣ idy : ${astro}\n\nPuntos *-${poin} XP*`)
         }
     } else {
         throw reseqv
