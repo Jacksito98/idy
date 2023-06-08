7881b2ab6d13f1e5aae26d6d9b9e94d2ec500aca
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-08 14:17:37 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,52 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+global.owner = [
+  ['18295176783', 'Jacksito', true],
+  ['1809995999'], 
+  ['529983385229'] 
+] //Numeros de owner 
+
+global.mods = ['529983385229'] 
+global.prems = ['50489079501', '5219631769130', '573125484672']
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz', 
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zenzapis.xyz': '675e34de8a', 
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+
+// Sticker WM
+global.packname = 'idy┃ᴮᴼᵀ' 
+global.author = ':3' 
+global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/Jack_paymon\n' 
+global.dygp = 'https://chat.whatsapp.com/FLnMCO1opLD8QU1lJiZMKj'
+global.fgsc = 'https://github.com' 
+global.fgyt = 'https://youtube.com'
+global.fgpyp = 'https://paypal.me'
+global.fglog = 'https://i.imgur.com/Owmb93c.png' 
+
+global.wait = '*⌛ _Cargando..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.xmoji = '🔥' 
+
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
