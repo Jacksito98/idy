a13a613e210278950c16d5028139b676b6878f2b
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-08 16:34:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['529983385229', 'Jacksito', true],
+  ['18295176783', 'Jacksito', true],
   ['1809995999'], 
-  ['18295176783'] 
+  ['529983385229'] 
 ] //Numeros de owner 
 
 global.mods = ['529983385229'] 
