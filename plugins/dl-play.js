ff71a1c329929234cae4c6264032840af1d8bd76
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-08 14:41:54 -0400

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -2,7 +2,7 @@
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 	
-	if (!text) throw `✳️ Ingresa el título de una canción\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life`
+	if (!text) throw `✳️ Ingresa el título de una canción\n\n📌Ejemplo *${usedPrefix + command}* Makes me wonder maroon5`
 	let res = await yts(text)
 	let vid = res.videos[0]
 	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
@@ -10,7 +10,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
 	m.react('🎧')
 	let play = `
-	≡ *FG MUSIC*
+	≡ *JS MUSIC*
 ┌──────────────
 ▢ 📌 *Título* : ${title}
 ▢ 📆 *Publicado:* ${ago}
@@ -18,8 +18,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 ▢ 👀 *Vistas:* ${views}
 └──────────────`
  await conn.sendButton(m.chat, play, fgig, thumbnail, [
-    ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
-    ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
+    ['🎶 MP3', `${usedPrefix}Jsmp3 ${url}`],
+    ['🎥 MP4', `${usedPrefix}jsmp4 ${url}`]
   ], m, rpl)
 }
 handler.help = ['play']
