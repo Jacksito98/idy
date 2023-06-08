1c55e2417bd1d13db52036ed0fc448d8fe7cb97e
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-08 15:27:32 -0400

    Add files via upload

---
+++ plugins/main-donate.js
@@ -0,0 +1,23 @@
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+
+    let don = `
+≡ *DONACION*
+puedes donar si quieres ayudar a mantener el bot activo
+
+▢ *PayPal*
+• *Link :* https://paypal.me/Jacksito23
+
+▢ *Ban reservas re.dom*
+• *Alias :* Jack A.
+• *N° :* ...
+`
+let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
+conn.sendFile(m.chat, img, 'img.jpg', don, m)
+
+}
+handler.help = ['donate']
+handler.tags = ['main']
+handler.command = ['apoyar', 'donate', 'donar'] 
+
+export default handler
