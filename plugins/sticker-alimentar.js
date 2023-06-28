67e858b2ebfc9edebcd257177a45a3c9c3aa0324
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-28 18:04:55 -0400

    Add files via upload

---
+++ plugins/sticker-alimentar.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch'
+import axios from 'axios'
+import { sticker } from '../lib/sticker.js'
+import MessageType from '@adiwajshing/baileys'
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+	
+	 let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
+    
+    let user = global.db.data.users[who]
+    let name = conn.getName(who) 
+   let name2 = conn.getName(m.sender) 
+   m.react(rwait)
+
+  let rki = await fetch(`https://nekos.life/api/v2/img/feed`)
+    if (!rki.ok) throw await rki.text()
+   let jkis = await rki.json()
+   let { url } = jkis
+   let stiker = await sticker(null, url, `(${name2}) Dio de comer a`, `${name}`)
+   conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+   m.react('🥄') 
+   
+}
+
+handler.help = ['food @tag']
+handler.tags = ['rnime']
+handler.command = /^(alimentar|food|alimentándose|alimentando|alimentar)$/i 
+handler.diamond = true
+handler.group = true
+
+export default handler
+
