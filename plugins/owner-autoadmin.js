05c88946564b52f2f9dd976709ec67d97d34b98f
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-06-28 12:51:51 -0400

    Add files via upload

---
+++ plugins/owner-autoadmin.js
@@ -0,0 +1,14 @@
+/* Creditos a https://github.com/unptoadrih15/UPABOT-MD */
+
+let handler = async (m, { conn, isAdmin }) => {  
+if (m.fromMe) return
+if (isAdmin) throw '*[❗] Hola creador, como esta? usted ya es admin en este grupo :)*'
+try {  
+await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
+} catch {
+await m.reply('*[❗] Error, no es posible darle admin*')}}
+handler.command = /^autoadmin$/i
+handler.rowner = true
+handler.group = true
+handler.botAdmin = true
+export default handler
