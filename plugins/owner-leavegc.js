04864b8b255a7098893f44cb23671b2de51b9f6e
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-12 05:08:50 -0400

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -2,6 +2,8 @@ let handler = async (m, {conn, args, command}) => {
   await m.reply("*Adios a todos, el Bot se despide! (≧ω≦)ゞ*");
   await conn.groupLeave(m.chat);
 };
+handler.help = ['leave']
+handler.tags = ['owner']
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i;
 handler.group = true;
 handler.rowner = true;
