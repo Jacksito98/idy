c314f7703509c052afdd0704b48ac39b428304b8
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-12 06:05:33 -0400

    Update Sticker▪amongus.js

--- plugins/Sticker▪amongus.js
+++ plugins/Sticker▪amongus.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 let handler = async (m, { conn }) => {
 let res = await fetch(`https://api.lolhuman.xyz/api/sticker/amongus?apikey=85faf717d0545d14074659ad`)
 let { url } = res
-conn.sendFile(m.chat, url, null, { asSticker: true }, m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })  
+conn.sendFile(m.chat, url, null, { asSticker: true }, m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: false, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })  
 
 }
 handler.help = ['smongus', 'stickersus']
