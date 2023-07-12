4408f4825a9c739e04de10d2b4879fbe74675bff
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-11 21:33:47 -0400

    Update fun-chiste.js

--- plugins/fun-chiste.js
+++ plugins/fun-chiste.js
@@ -2,7 +2,7 @@
 
 let handler = async (m, { conn, text}) => {
 
-m.reply(`╭┄〔 *${wm}* 〕┄⊱\n┊\n *😹 ${pickRandom(global.chiste)} 😹*\n┊\n*╰━━━⊰ 𓃠 ${vs} ⊱━━━━დ*`)
+m.reply(`└──✪ Dylux ┃ ᴮᴼᵀ ✪──`)
 }
 handler.tags = ['fun']
 handler.command = ['chiste']
