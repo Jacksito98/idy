c162eb830425e2f6671bbf10134333b4296b2817
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-18 10:49:41 -0400

    Update fun-medir.js

--- plugins/fun-medir.js
+++ plugins/fun-medir.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber cuando le mide ${command.replace('how', '')}*`
+if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber cuando le  ${command1.replace('how', '')}*`
 let textoco = `
 _*Le mide* *${Math.floor(Math.random() * 30)}%* *Cm a*_ *${text}* 🥵😏
 `
@@ -8,5 +8,6 @@ m.reply(textoco, null, {mentions: conn.parseMention(textoco)})
 handler.help = ['medir @user']
 handler.tags = ['fun']
 handler.command = /^(medir)/i
+handler.command1 = /^(mide)/i
 handler.fail = null
 export default handler
