5086885d4f481aab02282f87be1feca348a62a0a
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-18 09:49:26 -0400

    Add files via upload

---
+++ plugins/econ-pesc.js
@@ -0,0 +1,32 @@
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn }) => {
+
+  let hasil = Math.floor(Math.random() * 5000)
+  let time = global.db.data.users[m.sender].lastpesc + 14400000
+  if (new Date - global.db.data.users[m.sender].lastpesc < 14400000) throw `⏳ _Espera_ *${msToTime(time - new Date())}* _para regresar a pescar_`
+  global.db.data.users[m.sender].exp += hasil
+  m.react('🎣')
+  m.reply(`
+🎉 Genial! minaste *${hasil} XP*`)
+  global.db.data.users[m.sender].lastpesc = new Date * 1
+}
+handler.help = ['pescar']
+handler.tags = ['econ']
+handler.command = ['pescar', 'Pescar', 'pesc'] 
+
+export default handler
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+ 
+
+  return hours + " hora(s) " + minutes + " minuto(s) " + seconds + " segundo(s)" 
+}
