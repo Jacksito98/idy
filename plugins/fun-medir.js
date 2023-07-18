3f1291eb5ad4a9a8ee63a9edfed814ce92341647
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-18 10:38:40 -0400

    Add files via upload

---
+++ plugins/fun-medir.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, command, text }) => {
+if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber cuando le mide el pene ${command.replace('how', '')}*`
+let textoco = `
+_*te mide* *${Math.floor(Math.random() * 100)}%
+`
+m.reply(textoco, null, {mentions: conn.parseMention(textoco)})
+}
+handler.help = ['medir @user']
+handler.tags = ['fun']
+handler.command = /^(medir)/i
+handler.fail = null
+export default handler
