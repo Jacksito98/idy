f160add154e9dd252187b7d11207c0a1f5d034d1
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-18 09:26:00 -0400

    Update econ-mine.js

--- plugins/econ-mine.js
+++ plugins/econ-mine.js
@@ -25,7 +25,6 @@ function msToTime(duration) {
   hours = (hours < 10) ? "0" + hours : hours
   minutes = (minutes < 10) ? "0" + minutes : minutes
   seconds = (seconds < 10) ? "0" + seconds : seconds
-   m.react('⛏️')
-
+  
   return hours + " hora(s) " + minutes + " minuto(s) " + seconds + " segundo(s)" 
 }
