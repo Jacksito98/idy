df2eca44432483fa340ad75be272accc8744c1bf
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-18 10:40:55 -0400

    Update fun-medir.js

--- plugins/fun-medir.js
+++ plugins/fun-medir.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber cuando le mide el pene`
 let textoco = `
-_*te mide* *${Math.floor(Math.random() * 100)}%
+_*te mide* *${Math.floor(Math.random() * 100)}% cm
 `
 m.reply(textoco, null, {mentions: conn.parseMention(textoco)})
 }
