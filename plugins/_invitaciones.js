4aae7c8bcef419e044dbbca85f969350be0cca21
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-20 12:47:20 -0400

    Add files via upload

---
+++ plugins/_invitaciones.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+let handler = m => m
+
+handler.all = async function (m, { isBlocked }) {
+    if (isBlocked) return
+    // ketika ada yang invite/kirim link grup di chat pribadi
+    if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('invitacion a mi grupo de WhatsApp') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
+    let teks = `Para agregar al bot a tu grupo
+• Une a mi creador
+• Dale adm
+Y el creador agregara al bot a tu grupo cuando pueda, aqui te dejo a mi creador:)
+`
+    this.reply(m.chat, teks, m)
+    const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+    this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+    }
+}
+
+export default handler
