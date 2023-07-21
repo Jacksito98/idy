4437a2a529dae5f9b1502b7d654d627a842aa6fd
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-21 04:31:49 -0400

    Add files via upload

---
+++ plugins/downloader-playV2.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, {command, conn, text}) => {
+await conn.sendMessage(m.chat, { react: { text: '⏱️', key: m.key } })
+if (!text) throw `[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙾 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:\n#play.1 Good Feeling - Flo Rida*`
+try {
+if (command == 'play1') {
+conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴀᴜᴅɪᴏ...⏳_*`, m)  
+let res = await fetch("https://violetics.pw/api/media/youtube-play?apikey=beta&query="+text)
+let json = await res.json()
+conn.sendFile(m.chat, json.result.dlmp3, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
+if (command == 'play2') {
+conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*`, m)
+let res = await fetch("https://violetics.pw/api/media/youtube-play?apikey=beta&query="+text)
+let json = await res.json()
+conn.sendFile(m.chat, json.result.dlmp4, 'error.mp4', `_𝖀𝖓𝕻𝖙𝖔𝕬𝖉𝖗𝖎𝖍𝕭𝖔𝖙_`, m)}
+} catch (e) {
+m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*')
+}}
+handler.help = ['play.1' , 'play.2'].map(v => v + ' <texto>')
+handler.tags = ['downloader']
+handler.command = ['play1', 'play2']
+export default handler
