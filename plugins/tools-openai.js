c4fac6f308d0f29ea5efa08b32a9ca2c9932dbc7
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-21 19:26:48 -0400

    Add files via upload

---
+++ plugins/tools-openai.js
@@ -0,0 +1,22 @@
+
+import cheerio from 'cheerio';
+import gpt from 'api-dylux';
+let handler = async (m, { conn, text }) => {
+	
+if (!text) throw `✳️ Ingresa el texto`;
+m.react('💬')
+
+	try {
+        let syms = `Eres Idy Bot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
+        let res = await gpt.ChatGpt(text, syms)
+         await m.reply(res.text)
+	} catch {
+		m.reply(`❎ Error: intenta más tarde`);
+	}
+
+}
+handler.help = ['ia <text>']; 
+handler.tags = ['tools'];
+handler.command = ['ia', 'ai', 'chatgpt', 'openai', 'gpt'];
+
+export default handler;
