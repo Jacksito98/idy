668b59c629984df5f18a84ba9677f8d047081407
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-23 00:59:31 -0400

    Update sticker-trigger.js

--- plugins/sticker-trigger.js
+++ plugins/sticker-trigger.js
@@ -3,7 +3,7 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn }) => {
 	
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
+  let marah = global.API('https://some-random-api.com', '/canvas/triggered', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
   })
 let stiker = await sticker(false, marah, global.packname, global.author)
