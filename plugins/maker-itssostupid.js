f059bc039767e46614db45d58879304a75f18565
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-07-24 08:15:42 -0400

    Update maker-itssostupid.js

--- plugins/maker-itssostupid.js
+++ plugins/maker-itssostupid.js
@@ -4,7 +4,7 @@ let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.m
 conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/its-so-stupid', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
 dog: text || 'im+stupid'
-}), 'error.png','🔥 𝙀𝙎𝙏𝘼 𝙇𝙄𝙎𝙏𝙊!!\nIdy Bot', m)
+}), 'error.png','🔥 𝙀𝙎𝙏𝘼 𝙇𝙄𝙎𝙏𝙊!!\nＩＤＹ-ＢＯＴ ˃ʍ˂⁩', m)
 }  
 handler.help = ['itssostupid', 'iss', 'stupid']
 handler.tags = ['maker']
