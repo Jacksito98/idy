44e76ce4ba27252bd7af1029e36bb4eaa451eaac
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-08-21 14:09:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DyLux -  FG98', {
+say('Idy -  Jack98', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @fg98._`, {
+say(`'${name}' By @Jack_paymon._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
